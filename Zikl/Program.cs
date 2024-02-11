d83e4f68cd3589d178b49ae81a4383f4edc96caf
CORP-HR\natalya.berezovski <natalya.berezovski@hireright.com>
2024-02-11 22:31:31 +0200

    Add project files.

---
+++ Zikl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zikl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int cyсleNumbers;
+            Console.WriteLine("Сколько раз вы хотите, чтобы цикл повториться?");
+            cyсleNumbers= Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < cyсleNumbers; i++)
+            {
+                Console.WriteLine($" Этот цикл повторяется {i+1}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
